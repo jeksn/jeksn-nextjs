57dd81e74fded25e8ab1e5b63a84ddf4371c8d7e
Johan <johan@jeksn.me>
2024-11-21 21:04:18 +0100

    start on blog support

---
+++ app/blog/page.tsx
@@ -0,0 +1,13 @@
+import Hero from "../components/Hero";
+
+export default function Blog() {
+    
+    return (
+            <>
+            <Hero heading="Blog" subHeading="" />
+            <div className="max-w-screen-lg mx-auto px-4 my-12 markdown-format">
+                <p>This is the blog.</p>
+            </div>
+            </>
+        )
+    }
