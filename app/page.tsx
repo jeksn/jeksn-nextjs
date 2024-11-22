5fef1ca8171eaa716f1dd8e241e0e6f75728c6d6
Johan <johan@jeksn.me>
2024-11-22 21:46:04 +0100

    remove links from index page

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,8 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <section className="min-h-[60vh]">
-			<div className="flex flex-col items-center justify-start mt-[20vh] px-4">
+    <section>
+			<div className="flex flex-col items-center justify-center h-[70vh] px-4">
 					<div className="flex flex-col items-start justify-center gap-6">
 						<div className="flex flex-col text-base gap-y-1 md:text-xl">
 						  <h1 className="my-3 text-6xl font-light">Hello! 👋</h1>
@@ -21,15 +21,15 @@ export default function Home() {
                 Currently working at <Link href="https://wtmg.se" target="_blank" className="underline transition-all underline-offset-4 hover:underline-offset-8">WTMG.</Link>
               </p>
 					  </div>
-					<div className="mt-8">
-						<h2 className="text-lg md:text-xl">Where to find me</h2>
-						<ul className="mt-1 space-y-1 text-base">
-              <ContactLink href="https://github.com/jeksn" title="GitHub" />
-              <ContactLink href="https://www.linkedin.com/in/jeksn/" title="LinkedIn" />
-              <ContactLink href="https://read.cv/jeksn" title="Read.cv" />
-              <ContactLink href="mailto:johan@jeksn.me" title="E-mail me" />
-						</ul>
-					</div>
+            {/* <div className="mt-8">
+              <h2 className="text-lg md:text-xl">Where to find me</h2>
+              <ul className="mt-1 space-y-1 text-base">
+                <ContactLink href="https://github.com/jeksn" title="GitHub" />
+                <ContactLink href="https://www.linkedin.com/in/jeksn/" title="LinkedIn" />
+                <ContactLink href="https://read.cv/jeksn" title="Read.cv" />
+                <ContactLink href="mailto:johan@jeksn.me" title="E-mail me" />
+              </ul>
+            </div> */}
 						</div>
 				</div>
     </section>
